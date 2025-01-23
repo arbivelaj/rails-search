6fe2e0353ca8b245dadfc9eabf7d296e1d749eed
Arbi Velaj <arbi.velaj@gmail.com>
2025-01-23 10:08:29 +0100

    first commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,15 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+
+    if params[:query].present?
+      sql_subquery = <<~SQL
+        movies.title @@ :query
+        OR movies.synopsis @@ :query
+        OR directors.first_name @@ :query
+        OR directors.last_name @@ :query
+      SQL
+      @movies = @movies.joins(:director).where(sql_subquery, query: "%#{params[:query]}%")
+    end
+  end
+end
